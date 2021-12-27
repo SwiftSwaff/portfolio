6d28edea8009851d4ecc080e939a5da08f4116ab
SwiftSwaff <jeff.geyer.29@gmail.com>
2021-12-26 22:01:50 -0800

    Adjusted to use minified css, and added version no. for cache busting

--- components/imports.php
+++ components/imports.php
@@ -5,6 +5,6 @@ if (!defined('IndexAccessed')) {
 ?>
 
 <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-<link rel='stylesheet' href='/css/app.css'>
+<link rel='stylesheet' href='/css/app.min.css?v=1.0.0'>
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
 <script type='text/javascript' src='/js/app.js'></script>
