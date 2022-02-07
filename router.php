2b80be3eddc4041d16e287b790283a824fefc3c1
SwiftSwaff <jeff.geyer.29@gmail.com>
2022-02-06 22:24:59 -0800

    test2

--- router.php
+++ router.php
@@ -9,6 +9,7 @@ $projectRefs = array(
     "trading_game" => "trading_game.php",
 );
 //test1
+//test2
 // handle overview and project pages differently as projects may have multiple files associated to each
 switch ($slug) {
     case "":
